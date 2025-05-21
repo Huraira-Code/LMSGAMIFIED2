73ac876f71c569c894356e3dd69657bb30db655a
Huraira-Code <osama.shahid.k.pk@gmail.com>
2025-05-21 06:36:38 +0500

    adding database link

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,22 +3,20 @@ import mongoose from "mongoose";
 /**
  * @Connects to MongoDB database
  */
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
-const connectionToDB= async ()=>{
+const connectionToDB = async () => {
+  try {
+    const { connection } = await mongoose.connect(
+      "mongodb+srv://huraira:Usama10091@cluster0.hnawam1.mongodb.net/LMS"
+    );
 
-    try {
-        const{ connection}= await mongoose.connect(
-            process.env.MONGODB_URL||`mongodb+srv://huraira:Usama10091@cluster0.hnawam1.mongodb.net/LMS`
-        )
-    
-        if(connection){
-            console.log(`Connected to MongoDB :${connection.host}`);
-        }
-
-    } catch (e) {
-        console.log(e);
-        process.exit(1);
-    } 
-}
+    if (connection) {
+      console.log(`Connected to MongoDB :${connection.host}`);
+    }
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+};
 export default connectionToDB;
